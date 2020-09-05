8a35add8ba6631b6b736ed72984734a9fc537063
akromish <akram.weheba.11@gmail.com>
2020-09-05 13:37:00 -0400

    'extra'

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Med Tracker Sign In'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: RaisedButton(
+          child: Text('Sign In Anonymously'),
+          onPressed: () async {
+
+          },
+        ),
+      ),
+    );
+  }
+}
