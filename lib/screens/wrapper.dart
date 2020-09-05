f3aab72ba2a79ad385b74ac6fac4b71258658419
akromish <akram.weheba.11@gmail.com>
2020-09-05 12:38:23 -0400

    added structure for login and removed bloat

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Home();
+  }
+}
