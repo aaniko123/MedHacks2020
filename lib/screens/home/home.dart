9019911c032f646da520bfae8e2292311957c8b3
akromish <akram.weheba.11@gmail.com>
2020-09-05 20:15:04 -0400

    Home redirect

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/Drugs/drug.dart';
 import 'package:flutter_app/services/auth.dart';
 
 class Home extends StatelessWidget {
@@ -8,20 +9,33 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     backgroundColor: Colors.orange[50],
-      appBar: AppBar(
-        title: Text('Med Tracker'),
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('logout'))
-        ],
-      ),
+        backgroundColor: Colors.orange[50],
+        appBar: AppBar(
+          title: Text('Med Tracker Home'),
+          backgroundColor: Colors.blue[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('logout'))
+          ],
+        ),
+        body: Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
+            child: RaisedButton(
+                child: Text('Prescription Medication'),
+                onPressed: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Drug()),
+                  );
+                }
+            )
+        )
+
     );
   }
 }
